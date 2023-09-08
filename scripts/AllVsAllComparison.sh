6286ed2bc551d41edd3e6f7b78850efa6f48f505
Noa Brenner <100242315+brennern@users.noreply.github.com>
2023-09-08 15:34:49 +0900

    Update AllVsAllComparison.sh

--- scripts/AllVsAllComparison.sh
+++ scripts/AllVsAllComparison.sh
@@ -1,14 +1,26 @@
-#By Dr. Michael Mansfield and Dr. Charles Plessy at OIST
+#!/bin/sh
 
-cut -f 1,2 ../TAXON_NAME.tsv |
-  sed 1d |
-  while read ID FILE
-  do
-    nextflow run -r v5.2.0 oist/plessy_pairwiseGenomeComparison \
-      --input ../TAXON_NAME.tsv \
-      -profile oist \
-      -w /your/directory/to/store/results \
-      --target $FILE \
-      -resume
-    mv results results_$ID
+# We use the -resume option to avoid re-downloading all the files
+# But we purge the work directory from all the non-staging files
+# to save some space.
+
+cut -f 1,2 $1.tsv  |
+    sed 1d |
+    while read ID FILE
+    do
+      printf "$ID\n"
+      nextflow run oist/plessy_pairwiseGenomeComparison \
+	  -r lastal--split \
+          --input $1.tsv \
+          -profile oist \
+          -w /flash/LuscombeU/$(whoami)/nf_work_$1 \
+          --target $FILE \
+	  --targetName $ID \
+          -resume \
+          --skip_dotplot_1 \
+          --skip_dotplot_2 \
+          --skip_dotplot_3 \
+          --skip_m2m
+      rm -rf /flash/LuscombeU/$(whoami)/nf_work_$1/??
+      mv results results_$ID
   done
